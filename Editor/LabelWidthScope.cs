46c25a5bcac0d808ad4a786f8cfa02c219bb6926
baba_s <shotababa1206@gmail.com>
2020-08-26 08:58:08 +0900

    add scope

---
+++ Editor/LabelWidthScope.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEditor;
+
+namespace Kogane
+{
+	public sealed class LabelWidthScope : IDisposable
+	{
+		private readonly float m_oldLabelWidth;
+
+		public LabelWidthScope( int labelWidth )
+		{
+			m_oldLabelWidth             = EditorGUIUtility.labelWidth;
+			EditorGUIUtility.labelWidth = labelWidth;
+		}
+
+		public void Dispose()
+		{
+			EditorGUIUtility.labelWidth = m_oldLabelWidth;
+		}
+	}
+}
