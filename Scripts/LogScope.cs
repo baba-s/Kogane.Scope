f0654df60392a6afdaff045684712f411c987dc4
baba_s <shotababa1206@gmail.com>
2020-09-03 08:57:54 +0900

    add log scope

---
+++ Scripts/LogScope.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kogane
+{
+	/// <summary>
+	/// using ブロックの開始時と終了時にログを出力するクラス
+	/// </summary>
+	public sealed class LogScope : IDisposable
+	{
+		//==============================================================================
+		// デリゲート
+		//==============================================================================
+		public delegate void OnStartCallback( string message );
+
+		public delegate void OnCompleteCallback( string message );
+
+		//==============================================================================
+		// 変数(readonly)
+		//==============================================================================
+		private readonly string m_message;
+
+		//==============================================================================
+		// イベント(static)
+		//==============================================================================
+		public static OnStartCallback    OnStart    { get; set; } = message => Debug.Log( $"{message} 開始" );
+		public static OnCompleteCallback OnComplete { get; set; } = message => Debug.Log( $"{message} 終了" );
+
+		//==============================================================================
+		// 関数
+		//==============================================================================
+		/// <summary>
+		/// using ブロック開始時にログを出力します
+		/// </summary>
+		public LogScope( string message )
+		{
+			m_message = message;
+			OnStart?.Invoke( message );
+		}
+
+		/// <summary>
+		/// using ブロック終了時にログを出力します
+		/// </summary>
+		public void Dispose()
+		{
+			OnComplete?.Invoke( m_message );
+		}
+	}
+}
