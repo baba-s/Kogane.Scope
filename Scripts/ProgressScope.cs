bab5fc166f6154d284aafbd1066a867432167f5b
baba_s <shotababa1206@gmail.com>
2020-09-06 19:41:05 +0900

    add progress scope

---
+++ Scripts/ProgressScope.cs
@@ -0,0 +1,231 @@
+#if UNITY_2020_1_OR_NEWER
+
+using System;
+using UnityEditor;
+
+namespace Kogane
+{
+	public sealed class ProgressScope : IDisposable
+	{
+		private bool m_isDisposed;
+
+		public int Id { get; }
+
+		public ProgressScope
+		(
+			string           name,
+			string           description = null,
+			Progress.Options options     = Progress.Options.None,
+			int              parentId    = -1
+		)
+		{
+			Id = Progress.Start
+			(
+				name: name,
+				description: description,
+				options: options,
+				parentId: parentId
+			);
+		}
+
+		public bool Cancel()
+		{
+			return Progress.Cancel( Id );
+		}
+
+		public void ClearRemainingTime()
+		{
+			Progress.ClearRemainingTime( Id );
+		}
+
+		public bool Exists()
+		{
+			return Progress.Exists( Id );
+		}
+
+		public void Finish( Progress.Status status = Progress.Status.Succeeded )
+		{
+			Progress.Finish( Id, status );
+		}
+
+		public int GetCurrentStep()
+		{
+			return Progress.GetCurrentStep( Id );
+		}
+
+		public string GetDescription()
+		{
+			return Progress.GetDescription( Id );
+		}
+
+		public string GetName()
+		{
+			return Progress.GetName( Id );
+		}
+
+		public Progress.Options GetOptions()
+		{
+			return Progress.GetOptions( Id );
+		}
+
+		public int GetParentId()
+		{
+			return Progress.GetParentId( Id );
+		}
+
+		public int GetPriority()
+		{
+			return Progress.GetPriority( Id );
+		}
+
+		public float GetProgress()
+		{
+			return Progress.GetProgress( Id );
+		}
+
+		public Progress.Item GetProgressById()
+		{
+			return Progress.GetProgressById( Id );
+		}
+
+		public long GetRemainingTime()
+		{
+			return Progress.GetRemainingTime( Id );
+		}
+
+		public long GetStartDateTime()
+		{
+			return Progress.GetStartDateTime( Id );
+		}
+
+		public Progress.Status GetStatus()
+		{
+			return Progress.GetStatus( Id );
+		}
+
+		public string GetStepLabel()
+		{
+			return Progress.GetStepLabel( Id );
+		}
+
+		public Progress.TimeDisplayMode GetTimeDisplayMode()
+		{
+			return Progress.GetTimeDisplayMode( Id );
+		}
+
+		public int GetTotalSteps()
+		{
+			return Progress.GetTotalSteps( Id );
+		}
+
+		public long GetUpdateDateTime()
+		{
+			return Progress.GetUpdateDateTime( Id );
+		}
+
+		public bool IsCancellable()
+		{
+			return Progress.IsCancellable( Id );
+		}
+
+		public bool IsPausable()
+		{
+			return Progress.IsPausable( Id );
+		}
+
+		public bool Pause()
+		{
+			return Progress.Pause( Id );
+		}
+
+		public void RegisterCancelCallback( Func<bool> callback )
+		{
+			Progress.RegisterCancelCallback( Id, callback );
+		}
+
+		public void RegisterPauseCallback( Func<bool, bool> callback )
+		{
+			Progress.RegisterPauseCallback( Id, callback );
+		}
+
+		public void Report( float progress )
+		{
+			Progress.Report( Id, progress );
+		}
+
+		public void Report( int currentStep, int totalSteps )
+		{
+			Progress.Report( Id, currentStep, totalSteps );
+		}
+
+		public void Report( float progress, string description )
+		{
+			Progress.Report( Id, progress, description );
+		}
+
+		public void Report
+		(
+			int    currentStep,
+			int    totalSteps,
+			string description
+		)
+		{
+			Progress.Report( Id, currentStep, totalSteps, description );
+		}
+
+		public bool Resume()
+		{
+			return Progress.Resume( Id );
+		}
+
+		public void SetDescription( string description )
+		{
+			Progress.SetDescription( Id, description );
+		}
+
+		public void SetPriority( Progress.Priority priority )
+		{
+			Progress.SetPriority( Id, priority );
+		}
+
+		public void SetPriority( int priority )
+		{
+			Progress.SetPriority( Id, priority );
+		}
+
+		public void SetRemainingTime( long seconds )
+		{
+			Progress.SetRemainingTime( Id, seconds );
+		}
+
+		public void SetStepLabel( string label )
+		{
+			Progress.SetStepLabel( Id, label );
+		}
+
+		public void SetTimeDisplayMode( Progress.TimeDisplayMode displayMode )
+		{
+			Progress.SetTimeDisplayMode( Id, displayMode );
+		}
+
+		public void UnregisterCancelCallback()
+		{
+			Progress.UnregisterCancelCallback( Id );
+		}
+
+		public void UnregisterPauseCallback()
+		{
+			Progress.UnregisterCancelCallback( Id );
+		}
+
+		public void Dispose()
+		{
+			if ( m_isDisposed ) return;
+			m_isDisposed = true;
+
+			Progress.Remove( Id );
+		}
+	}
+}
+
+#endif
